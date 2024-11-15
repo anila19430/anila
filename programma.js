b117120c0be2da91a615e055fc3fe0fdd7e851b0
anila19430 <ilaria.giordano@iismontalegenova.it>
2024-11-15 10:36:10 +0100

    ciao

--- programma.js
+++ programma.js
@@ -8,10 +8,35 @@ disegno.fillStyle = "pink"
 
 disegno.fillRect(199,199, 100, 50)
  
-disegno.fillStyle = "red"
+disegno.fillStyle = "violet"
 
 disegno.fillRect(200, 200, 50, 100) 
 
-disegno.fillStyle = "blue"
+disegno.fillStyle = " blue"
 
 disegno.fillRect(230, 230, 50, 100)
+
+disegno.fillStyle = "black"
+
+disegno.fillRect(250, 0, 10, 500)
+
+disegno.fillStyle = "black"
+
+disegno.fillRect(0, 250, 500, 10)
+
+disegno.fillStyle = "yellow"
+
+disegno.fillRect(260, 0, 250, 250)
+
+disegno.fillStyle = "pink"
+
+disegno.fillRect(0, 0, 250, 250)
+
+let foglio2 = document.getElementById("foglio2");
+
+let disegno2 = foglio2.getContext("2d");
+
+foglio2.addEventListener('click', (evento)=> {
+    disegno2.strokeRect (evento.offsetX, evento.offsetY, 500* Math.random(), 500 * Math.random());
+
+});
